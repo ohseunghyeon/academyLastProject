39140d3a0d007ca355cdb3c6f744dee53d5a110a
woojungeom <skywjlove@naver.com>
2016-01-27 16:20:07 +0900

    버전0.0.3
    DB 메뉴 리스트 호출

--- realLastProject/src/main/java/com/first/lastproject/controller/EomController.java
+++ realLastProject/src/main/java/com/first/lastproject/controller/EomController.java
@@ -1,12 +1,26 @@
 package com.first.lastproject.controller;
 
-import org.springframework.stereotype.Controller;
+import javax.servlet.http.HttpServletRequest;
 
-/**
- * Handles requests for the application home page.
- */
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.first.lastproject.command.goods.GoodsCommand;
+import com.first.lastproject.command.goods.GoodsListCommand;
+
 @Controller
 public class EomController {
 	
-
+	@RequestMapping("/GoodsList")
+	public String GoodsList(HttpServletRequest request, Model model) {
+		
+		model.addAttribute("request", request);
+		
+		GoodsCommand command = new GoodsListCommand();
+		
+		String viewName = command.execute(model);
+		
+		return viewName;
+	}
 }
--- realLastProject/src/main/java/com/first/lastproject/dao/food/FoodDao.java
+++ realLastProject/src/main/java/com/first/lastproject/dao/food/FoodDao.java
@@ -1,5 +1,12 @@
 package com.first.lastproject.dao.food;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.sql.DataSource;
@@ -27,10 +34,45 @@ public class FoodDao implements InterfaceFoodDao {
 			e.printStackTrace();
 		}
 	}
+
 	@Override
-	public GoodsDto getGoods() {
-		// TODO 자동 생성된 메소드 스텁
-		return new GoodsDto();
+	public List<GoodsDto> goods() {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		ArrayList<GoodsDto> goodslist = null;
+		
+		try {
+			con = dataSource.getConnection(); //컨넥션풀에서 connection객체 가져온다.
+			String sql = "select * from p_food";
+			pstmt = con.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			while(rs.next()) {
+				if(goodslist == null) {
+					goodslist = new ArrayList<GoodsDto>();
+				}
+				GoodsDto goods = new GoodsDto(); 
+			goods.setFood_code(rs.getInt(1));
+			goods.setFood_name(rs.getString(2));
+			goods.setPrice(rs.getInt(3));
+			goods.setFood_num(rs.getInt(4));
+			goodslist.add(goods);
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(rs!=null) rs.close();
+				if(pstmt != null) pstmt.close();
+				if(con != null) con.close();
+			} catch (SQLException e2) {
+				e2.printStackTrace();
+			}
+		}
+		return goodslist;
 	}
+
+		
 
 }
