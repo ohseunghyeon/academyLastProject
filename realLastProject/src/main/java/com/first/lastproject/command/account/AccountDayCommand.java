682ba4bd2a6862220a6772a77c61ebe5981728cf
ojneo16 <ojn2003@naver.com>
2016-01-29 21:56:38 +0900

    version 0.3.5 일간 정산 메뉴 컬럼들 출력 확인. 월간 정산과 기간 정산은 아직 진행되지 못함. 큰일이다...

--- realLastProject/src/main/java/com/first/lastproject/command/account/AccountDayCommand.java
+++ realLastProject/src/main/java/com/first/lastproject/command/account/AccountDayCommand.java
@@ -1,18 +1,36 @@
 package com.first.lastproject.command.account;
 
+ 
+import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.ui.Model;
 
+import com.first.lastproject.dao.account.AccountDao;
+import com.first.lastproject.dao.account.InterfaceAccountDao;
+import com.first.lastproject.dto.AccountDto;
+
 public class AccountDayCommand implements AccountCommand {
 
 	@Override
 	public String execute(Model model) {
-		Map <String, Object> map = model.asMap();
-		HttpServletRequest request = (HttpServletRequest)map.get("request");
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG);
+		AccountDto dto = new AccountDto();
+		String formattedDate = dateFormat.format(date);	
+		InterfaceAccountDao dao = AccountDao.getInstance();
+		List <AccountDto> list = new ArrayList<AccountDto>();
+		dto = dao.getDayTotalAccount();
 		
+		list = dao.getDayAccount();
+		model.addAttribute("list", list);
+		model.addAttribute("dto", dto);
+		model.addAttribute("time", formattedDate );
 		return "/account/accountDay";
 	}
 
