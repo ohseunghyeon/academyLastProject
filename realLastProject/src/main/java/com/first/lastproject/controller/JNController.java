b1e180367d7e94246935ee128c17922c7b99c988
SeungHyeonOh <postercolo@naver.com>
2016-02-22 14:38:23 +0900

    pruned source

--- realLastProject/src/main/java/com/first/lastproject/controller/JNController.java
+++ realLastProject/src/main/java/com/first/lastproject/controller/JNController.java
@@ -20,7 +20,6 @@ import com.first.lastproject.command.account.AccountMonthCommand;
 import com.first.lastproject.command.email.Email;
 import com.first.lastproject.command.email.EmailSender;
 import com.first.lastproject.command.member.HostLoginProCommand;
-import com.first.lastproject.command.member.MemberCommand;
 import com.first.lastproject.command.member.MemberConfirmIdCommand;
 import com.first.lastproject.command.member.MemberFindProCommand;
 import com.first.lastproject.command.member.MemberLoginFormCommand;
@@ -31,12 +30,11 @@ import com.first.lastproject.command.member.MemberModifyViewCommand;
 import com.first.lastproject.command.member.MemberRegisterProCommand;
 import com.first.lastproject.command.member.NoMemberLoginProCommand;
 import com.first.lastproject.dao.MemberDao;
-import com.first.lastproject.dao.MemberDaoImpl;
 import com.first.lastproject.dto.MemberDto;
 
 @Controller
 public class JNController {
-	
+
 	@RequestMapping("/index")
 	public String index(Model model) {
 		return "/index";
@@ -46,172 +44,188 @@ public class JNController {
 	public String memberMain(Model model) {
 		return "/member/memberMain";
 	}
-	
+
 	@RequestMapping("/memberRegister")
 	public String memberRegister(Model model) {
 		return "/member/memberRegister";
-	}	
-	
+	}
+
 	@Autowired
 	MemberConfirmIdCommand memberConfirmIdCommand;
-	@RequestMapping("/memberConfirmId")	
+
+	@RequestMapping("/memberConfirmId")
 	public String memberConfirmId(HttpServletRequest request, Model model) {
 		model.addAttribute("request", request);
 		String viewname = memberConfirmIdCommand.execute(model);
 		return viewname;
 	}
-	
+
 	@Autowired
 	MemberRegisterProCommand memberRegisterProCommand;
-	@RequestMapping("/memberRegisterPro")	
+
+	@RequestMapping("/memberRegisterPro")
 	public String memberRegisterPro(HttpServletRequest request, Model model) {
 		model.addAttribute("request", request);
 		String viewname = memberRegisterProCommand.execute(model);
 		return viewname;
 	}
-	
+
 	@Autowired
 	MemberLoginFormCommand memberLoginFormCommand;
-	@RequestMapping("/memberLoginForm")	
+
+	@RequestMapping("/memberLoginForm")
 	public String memberLoginForm(HttpServletRequest request, Model model) {
 		model.addAttribute("request", request);
 		String viewname = memberLoginFormCommand.execute(model);
 		return viewname;
 	}
-	
+
 	@Autowired
 	MemberLoginProCommand memberLoginProCommand;
-	@RequestMapping("/memberLoginPro")	
+
+	@RequestMapping("/memberLoginPro")
 	public String memberLoginPro(HttpServletRequest request, Model model) {
 		model.addAttribute("request", request);
 		String viewname = memberLoginProCommand.execute(model);
 		return viewname;
 	}
-	
+
 	@Autowired
 	NoMemberLoginProCommand noMemberLoginProCommand;
-	@RequestMapping("/nomemberLoginPro")	
+
+	@RequestMapping("/nomemberLoginPro")
 	public String nomemberLoginPro(HttpServletRequest request, Model model) {
 		model.addAttribute("request", request);
 		String viewname = noMemberLoginProCommand.execute(model);
 		return viewname;
 	}
-	
-	@RequestMapping("/hostLoginForm")	
+
+	@RequestMapping("/hostLoginForm")
 	public String hostLoginForm(Model model) {
 		return "/member/hostLoginForm";
 	}
-	
+
 	@Autowired
 	HostLoginProCommand hostLoginProCommand;
-	@RequestMapping("/hostLoginPro")	
+
+	@RequestMapping("/hostLoginPro")
 	public String hostLoginPro(HttpServletRequest request, Model model) {
 		model.addAttribute("request", request);
 		String viewname = hostLoginProCommand.execute(model);
 		return viewname;
 	}
+
 	@Autowired
 	MemberLogoutCommand memberLogoutCommand;
-	@RequestMapping("/memberLogout")	
+
+	@RequestMapping("/memberLogout")
 	public String memberLogout(HttpServletRequest request, Model model) {
 		model.addAttribute("request", request);
 		String viewname = memberLogoutCommand.execute(model);
 		return viewname;
 	}
-	
+
 	@RequestMapping("/memberFindForm")
 	public String memberFindForm(Model model) {
 		return "/member/memberFindForm";
 	}
+
 	@Autowired
 	MemberFindProCommand memberFindProCommand;
+
 	@RequestMapping("/memberFindPro")
 	public String memberFindPro(HttpServletRequest request, Model model) {
 		model.addAttribute("request", request);
 		String viewname = memberFindProCommand.execute(model);
 		return viewname;
 	}
-	
-	  @Autowired
-      private EmailSender emailSender;
-      @Autowired
-      private Email email;
-      @Autowired
-      MemberDao dao;
-       @RequestMapping("/passwdMail")
-       public ModelAndView sendEmailAction (@RequestParam Map<String, Object> map, ModelMap model) throws Exception {
-           ModelAndView mv;
-           String id=(String)map.get("id");
-           String emails=(String) map.get("email");
-           Map <String, String> map2 = new HashMap<String, String>();
-           map2.put("id", id);
-           map2.put("email", emails);
-           System.out.println(id);
-           System.out.println(emails);
-           
-           //비밀번호 가져오기
-           MemberDto dto = dao.getPasswd(map2);
-           String passwd = dto.getPasswd();
-           
-           if(passwd!=null) {
-               email.setContent("비밀번호는 "+passwd+" 입니다.");
-               email.setReceiver(emails);
-               email.setSubject(id+"님 비밀번호 찾기 메일입니다.");
-               emailSender.SendEmail(email);
-               int idorpass = 1;
-               model.addAttribute("id", id);
-               model.addAttribute("email", emails);
-               model.addAttribute("result", 1);
-               model.addAttribute("idorpass", idorpass);
-               mv= new ModelAndView("member/memberFindPro");
-               return mv;
-           } else {
-               model.addAttribute("result", 0);
-               mv=new ModelAndView("member/memberFindPro");
-               return mv;
-           }
-       }
 
-	
+	@Autowired
+	private EmailSender emailSender;
+	@Autowired
+	private Email email;
+	@Autowired
+	MemberDao dao;
+
+	@RequestMapping("/passwdMail")
+	public ModelAndView sendEmailAction(@RequestParam Map<String, Object> map, ModelMap model) throws Exception {
+		ModelAndView mv;
+		String id = (String) map.get("id");
+		String emails = (String) map.get("email");
+		Map<String, String> map2 = new HashMap<String, String>();
+		map2.put("id", id);
+		map2.put("email", emails);
+		System.out.println(id);
+		System.out.println(emails);
+
+		// 비밀번호 가져오기
+		MemberDto dto = dao.getPasswd(map2);
+		String passwd = dto.getPasswd();
+
+		if (passwd != null) {
+			email.setContent("비밀번호는 " + passwd + " 입니다.");
+			email.setReceiver(emails);
+			email.setSubject(id + "님 비밀번호 찾기 메일입니다.");
+			emailSender.SendEmail(email);
+			int idorpass = 1;
+			model.addAttribute("id", id);
+			model.addAttribute("email", emails);
+			model.addAttribute("result", 1);
+			model.addAttribute("idorpass", idorpass);
+			mv = new ModelAndView("member/memberFindPro");
+			return mv;
+		} else {
+			model.addAttribute("result", 0);
+			mv = new ModelAndView("member/memberFindPro");
+			return mv;
+		}
+	}
+
 	@RequestMapping("/memberModifyForm")
 	public String memberModifyForm(Model model) {
 		return "/member/memberModifyForm";
 	}
+
 	@Autowired
 	MemberModifyViewCommand memberModifyViewCommand;
+
 	@RequestMapping("/memberModifyView")
 	public String memberModifyView(HttpServletRequest request, Model model) {
 		model.addAttribute("request", request);
 		String viewname = memberModifyViewCommand.execute(model);
 		return viewname;
 	}
+
 	@Autowired
 	MemberModifyProCommand memberModifyProCommand;
-	@RequestMapping("/memberModifyPro")	
+
+	@RequestMapping("/memberModifyPro")
 	public String memberModifyPro(HttpServletRequest request, Model model) {
 		model.addAttribute("request", request);
 		String viewname = memberModifyProCommand.execute(model);
 		return viewname;
 	}
-	
+
 	@Autowired
 	AccountDayCommand accountDayCommand;
+
 	@RequestMapping("/accountDay")
 	public String accountDay(HttpServletRequest request, Model model) {
 		model.addAttribute("request", request);
 		String viewname = accountDayCommand.execute(model);
 		return viewname;
 	}
-	
+
 	@Autowired
 	AccountMonthCommand accountMonthCommand;
+
 	@RequestMapping("/accountMonth")
 	public String accountMonth(HttpServletRequest request, Model model) {
 		model.addAttribute("request", request);
 		String viewname = accountMonthCommand.execute(model);
 		return viewname;
 	}
-	
+
 	@RequestMapping("/accountDateSelect")
 	public String accountDateSelect(HttpServletRequest request, Model model) {
 		model.addAttribute("request", request);
